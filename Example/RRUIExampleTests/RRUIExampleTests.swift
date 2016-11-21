311bb3738660ec6fc6c0eadace056be86def8bb4
Sambhav Shah <sambhav.shah@practo.com>
2016-11-21 17:04:50 +0530

    Updated Example

--- Example/RRUIExampleTests/RRUIExampleTests.swift
+++ Example/RRUIExampleTests/RRUIExampleTests.swift
@@ -1,7 +1,15 @@
-import UIKit
-import XCTest
+//
+//  RRUIExampleTests.swift
+//  RRUIExampleTests
+//
+//  Created by Sambhav Shah on 21/11/16.
+//  Copyright © 2016 Compan. All rights reserved.
+//
 
-class Tests: XCTestCase {
+import XCTest
+@testable import RRUIExample
+
+class RRUIExampleTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
@@ -15,12 +23,12 @@ class Tests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure() {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
